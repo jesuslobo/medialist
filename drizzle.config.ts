a655dfa5404555c45058b1a47ab1915309fc7efb
khalidibnwalid <kh.hawajreh@gmail.com>
2024-07-24 19:46:25 +0300

    initialized drizzle and sqlite

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    dialect: "sqlite",
+    schema: "./src/db/schema.ts",
+    out: "./src/db/migrations",
+    dbCredentials: {
+        url: './sqlite.db',
+    },
+    verbose: true,
+    strict: true,
+})
