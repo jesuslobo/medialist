8a8091f2209ec697c7b28c791d2e70802a004834
khalidibnwalid <kh.hawajreh@gmail.com>
2024-08-12 22:14:23 +0300

    created items db schema and typescript interface

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,29 +1,68 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
-    id: text("id").notNull().primaryKey(),
-    username: text("username").notNull().unique(),
-    passwordHash: text("password_hash").notNull(),
+    id: text("id")
+        .notNull()
+        .primaryKey(),
+    username: text("username")
+        .notNull()
+        .unique(),
+    passwordHash: text("password_hash")
+        .notNull(),
     // createdAt: integer("created_at").notNull(),
     // updatedAt: integer("updated_at").notNull(),
 });
 
 export const sessionsTable = sqliteTable("sessions", {
-    id: text("id").notNull().primaryKey(),
+    id: text("id")
+        .notNull()
+        .primaryKey(),
     userId: text("user_id")
         .notNull()
         .references(() => usersTable.id),
-    expiresAt: integer("expires_at").notNull()
+    expiresAt: integer("expires_at")
+        .notNull()
 });
 
 export const listsTable = sqliteTable("lists", {
-    id: text("id").notNull().primaryKey(),
+    id: text("id")
+        .notNull()
+        .primaryKey(),
     userId: text("user_id")
         .notNull()
         .references(() => usersTable.id),
-    title: text("title").notNull(),
+    title: text("title")
+        .notNull(),
     coverPath: text("cover_path"),
-    trash: integer("trash", { mode: "boolean" }).notNull().default(false),
+    trash: integer("trash", { mode: "boolean" })
+        .notNull()
+        .default(false),
+    // fav: integer("fav", { mode: "boolean" }).notNull().default(false),
+    // createdAt: integer("created_at").notNull(),
+    // updatedAt: integer("updated_at").notNull(),
+    // templates: text("templates").notNull().default("[]"), // JSON string
+    // configs: text("templates").notNull().default("[]"), // JSON string
+    // apis: text("templates").notNull().default("[]"), // JSON string
+});
+
+export const itemsTable = sqliteTable("items", {
+    id: text("id")
+        .notNull()
+        .primaryKey(),
+    userId: text("user_id")
+        .notNull()
+        .references(() => usersTable.id),
+    listId: text("list_id")
+        .notNull()
+        .references(() => listsTable.id),
+    title: text("title")
+        .notNull(),
+    posterPath: text("poster_path"),
+    coverPath: text("cover_path"),
+    description: text("description"),
+    trash: integer("trash", { mode: "boolean" })
+        .notNull()
+        .default(false),
     // fav: integer("fav", { mode: "boolean" }).notNull().default(false),
     // createdAt: integer("created_at").notNull(),
     // updatedAt: integer("updated_at").notNull(),
--- src/utils/types/item.ts
+++ src/utils/types/item.ts
@@ -1,65 +1,67 @@
-import { TagData } from "./global";
-
-export interface itemImageType {
-    id: string
-    image_path: string
-    title?: string;
-    description?: string;
-    item_id: string
-}
+import { UserData } from "./global"
+import { ListData } from "./list"
 
 export interface ItemData {
-    id: string;
-    list_id: string;
-    title: string;
-    poster_path?: string
-    cover_path?: string
-    description?: string;
-    fav?: boolean;
-    trash?: boolean;
-    tags: TagData['id'][]
-    // progress_state?: itemProgressState;
-    links?: Itemlink[]
+    id: string
+    userID: UserData['id']
+    listId: ListData['id']
+    title: string
+    posterPath?: string
+    coverPath?: string
+    description?: string
+    // fav?: boolean
+    trash?: boolean
+    // tags: Tag['id'][]
+    // progress_state?: itemProgressState
+    // links?: Itemlink[]
     // content_fields?: itemField[]
     // main_fields?: main_fields[]
-    // badges?: itemBadgesType[];
-    related?: ItemData['id'][]
+    // badges?: itemBadgesType[]
+    // related?: ItemData['id'][]
     // configurations: ItemConfiguration
     // extra_fields?: { name: string, value?: string }[]
 }
 
+export interface ItemImageData {
+    id: string
+    image_path: string
+    title?: string
+    description?: string
+    item_id: string
+}
+
 export interface ItemMainFields {
-    name: string;
-    value: string | number;
-    itemId?: string;
-    bIsNumber?: boolean;
+    name: string
+    value: string | number
+    itemId?: string
+    bIsNumber?: boolean
 
 }
 
 // interface ItemConfiguration {
-//     layout?: "1" | "2" | "3" | "4";
+//     layout?: "1" | "2" | "3" | "4"
 // }
 
 
 export interface Itemlink {
-    url?: string;
-    logo_path: string;
+    url?: string
+    logo_path: string
     label: string
 }
 
 
 export interface ItemBadgesType {
-    logo_path: string;
-    label: string;
+    logo_path: string
+    label: string
 }
 
 // interface ItemField {
-//     label: string;
+//     label: string
 //     body: string
 // }
 
 // export interface itemProgressState {
 //     /**NextUI colors */
-//     color: "default" | "primary" | "secondary" | "success" | "warning" | "danger" | undefined;
+//     color: "default" | "primary" | "secondary" | "success" | "warning" | "danger" | undefined
 //     name: string
 // }
