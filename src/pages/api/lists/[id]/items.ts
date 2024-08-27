fa7bfdee633e11e5ae37c9ed7ff147283071614d
khalidibnwalid <kh.hawajreh@gmail.com>
2024-08-27 16:38:05 +0300

    moved the "POST item to a list" endpoint from api/items/[id] to api/lists/[id]/items for consistency with tags POST - tags are added to a list with POST via the api/lists/[id]/tags endpoints, so I moved the items POST to be in api/lists/[id]/items

--- src/pages/api/lists/[id]/items.ts
+++ src/pages/api/lists/[id]/items.ts
@@ -1,19 +1,25 @@
 import { db } from '@/db';
-import { itemsTable, listsTable } from '@/db/schema';
+import { itemsTable } from '@/db/schema';
 import { validateAuthCookies } from '@/utils/lib/auth';
-import { validatedID } from '@/utils/lib/generateID';
+import handleFileUpload from '@/utils/lib/fileHandling/handleFileUpload';
+import { coverThumbnailsOptions } from '@/utils/lib/fileHandling/thumbnailOptions';
+import { generateID, validatedID } from '@/utils/lib/generateID';
+import { ItemData } from '@/utils/types/item';
 import { ListData } from '@/utils/types/list';
+import busboy from 'busboy';
 import { and, eq } from 'drizzle-orm';
+import { mkdir } from 'fs/promises';
 import type { NextApiRequest, NextApiResponse } from 'next';
-
+import { join } from 'path';
 
 /** api/lists/[id]/items
  * Get: Get all items of a list
+ * Post: creates an item in list the list
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const { id: listID } = req.query;
-        if (!validatedID(listID)) return res.status(400).json({ message: 'Bad Request' });
+        const { id: listId } = req.query as { id: ListData['id'] };
+        if (!validatedID(listId)) return res.status(400).json({ message: 'Bad Request' });
 
         const { user } = await validateAuthCookies(req, res);
         if (!user) return res.status(401).json({ message: 'Unauthorized' });
@@ -24,11 +30,62 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 .from(itemsTable)
                 .where(and(
                     eq(itemsTable.userId, user.id),
-                    eq(itemsTable.listId, listID as ListData['id']),
+                    eq(itemsTable.listId, listId),
                     eq(itemsTable.trash, false)
                 ));
 
             return res.status(200).json(items);
+        }
+
+        if (req.method === 'POST') {
+            const itemId = generateID()
+
+            let data: Partial<ItemData> = {
+                id: itemId,
+                userId: user.id,
+                listId,
+            }
+
+            const itemDir = join('public', 'users', user.id, listId, itemId);
+            const thumbnailsDir = join(itemDir, 'thumbnails')
+            await mkdir(thumbnailsDir, { recursive: true });
+
+            const bb = busboy({ headers: req.headers })
+
+            bb.on('file', (name, file, info) => {
+                if (name === 'posterPath')
+                    data.posterPath = handleFileUpload(file, itemDir, {
+                        thumbnails: coverThumbnailsOptions.itemPoster,
+                        fileName: info.filename
+                    })
+
+                if (name === 'coverPath')
+                    data.coverPath = handleFileUpload(file, itemDir, {
+                        thumbnails: coverThumbnailsOptions.itemCover,
+                        fileName: info.filename
+                    })
+            })
+
+            bb.on('field', (name, val) => {
+                if (name === 'title') data.title = val;
+                if (name === 'description') data.description = val;
+            })
+
+            bb.on('close', async () => {
+                if (!data.title) return res.status(400).json({ message: 'Invalid Request' });
+
+                const item = await db.insert(itemsTable).values({
+                    id: itemId,
+                    listId,
+                    userId: user.id,
+                    title: data.title,
+                    coverPath: data.coverPath,
+                    posterPath: data.posterPath,
+                }).returning();
+
+                res.status(201).json(item[0]);
+            })
+            return req.pipe(bb)
         }
 
         res.status(405).json({ message: 'Method Not Allowed' });
@@ -37,3 +94,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(500).json({ message: 'Internal Server Error' });
     }
 }
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
