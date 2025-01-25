77228acd75181b4da169f9e8fdf4ebed456deced
khalidibnwalid <kh.hawajreh@gmail.com>
2025-01-25 08:36:51 +0300

    test(api/users): add tests for api/users GET, POST, and PATCH endpoint - Create $mockUser() to create a temp user, with utility methods     - returns userData, delete, createCockie, and userDir

---
+++ __tests__/api/users/users.node.test.ts
@@ -0,0 +1,173 @@
+import usersRoute from '@/pages/api/users';
+import { db } from '@/server/db';
+import { usersTable } from '@/server/db/schema';
+import { $verifyPassword } from '@/server/utils/auth/auth';
+import { generateID } from '@/utils/lib/generateID';
+import $mockUser from '@tests/test-utils/mocks/data/mockUser';
+import $mockHttp from '@tests/test-utils/mocks/mockHttp';
+import { eq } from 'drizzle-orm';
+import { fs, vol } from 'memfs';
+import { join } from 'path';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+
+vi.mock('node:fs')
+vi.mock('node:fs/promises')
+vi.mock('@/server/db');
+
+beforeEach(() => vol.reset())
+
+describe('api/users/', () => {
+    describe('GET - get userdata', () => {
+        test('should get userdata via cookies', async () => {
+            const { userData: { username, id, createdAt, updatedAt }, ...user } = await $mockUser()
+            const { cookies } = await user.createCookie()
+            const { body, statusCode } = await $mockHttp(usersRoute).get(undefined, { cookies });
+
+            expect(body).toEqual({
+                id,
+                username,
+                createdAt: createdAt.toISOString(),
+                updatedAt: updatedAt.toISOString(),
+            })
+            expect(statusCode).toBe(200)
+
+            await user.delete()
+        })
+
+        test('shouldn\'t get userdata without cookies', async () => {
+            const { body, statusCode } = await $mockHttp(usersRoute).get();
+
+            expect(body).toEqual({ message: 'Unauthorized' })
+            expect(statusCode).toBe(401)
+        })
+    })
+
+    describe('POST - register a new user', () => {
+        test('should create a new user', async () => {
+            const username = generateID(5);
+            const password = generateID(6);
+
+            const { body, statusCode, headers: reqHeaders } = await $mockHttp(usersRoute).post({ username, password });
+
+            expect(body).toEqual({
+                id: expect.stringMatching(/^[123456789ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghjklmnpqrstuvwxyz]{10}$/),
+                username: username.toLowerCase(),
+            })
+            expect(statusCode).toBe(201)
+            expect(reqHeaders).toHaveProperty('set-cookie')
+
+            const userDir = join('public', 'users', body.id)
+            const userDirExists = fs.existsSync(userDir)
+
+            expect(userDirExists).toBe(true)
+
+            await db.delete(usersTable).where(eq(usersTable.username, username.toLowerCase())).limit(1)
+        })
+
+        test('shouldn\'t create a new user with an existing username', async () => {
+            const { userData: { username, password }, ...user } = await $mockUser()
+            const { body, statusCode } = await $mockHttp(usersRoute).post({ username, password });
+
+            expect(body).toEqual({
+                cause: { username: "User Already Exists" },
+                message: 'Invalid Request'
+            })
+            expect(statusCode).toBe(400)
+
+            await user.delete()
+        })
+
+        test('shouldn\'t create a new user without a username', async () => {
+            const { body, statusCode } = await $mockHttp(usersRoute).post({ password: 'testpassword' });
+
+            expect(body).toEqual({ message: 'Invalid Request' })
+            expect(statusCode).toBe(400)
+        })
+
+        test('shouldn\'t create a new user without a password', async () => {
+            const { body, statusCode } = await $mockHttp(usersRoute).post({ username: 'testusername' });
+
+            expect(body).toEqual({ message: 'Invalid Request' })
+            expect(statusCode).toBe(400)
+        })
+    })
+
+    describe('PATCH - change userdata', () => {
+        test('should change username', async () => {
+            const oldDate = new Date(2000, 1)
+            const { userData: { id, createdAt, updatedAt }, ...user } = await $mockUser(undefined, undefined, oldDate)
+            const { cookies } = await user.createCookie()
+            const newUsername = generateID(5)
+
+            const { body, statusCode } = await $mockHttp(usersRoute).patch({ newUsername }, { cookies });
+
+            expect(body).toEqual({
+                id,
+                username: newUsername.toLowerCase(),
+                createdAt: createdAt.toISOString(),
+                updatedAt: expect.not.stringMatching(updatedAt.toISOString()),
+            })
+
+            expect(statusCode).toBe(200)
+            await user.delete()
+        })
+
+        test('should change password with a valid old password', async () => {
+            const oldDate = new Date(2000, 1)
+            const { userData, ...user } = await $mockUser(undefined, undefined, oldDate)
+            const { id, createdAt, updatedAt, password, username } = userData
+            const { cookies } = await user.createCookie()
+            const newPassword = generateID(6)
+
+            const { body, statusCode } = await $mockHttp(usersRoute).patch({ newPassword, oldPassword: password }, { cookies });
+
+            const newUser = await db.select().from(usersTable).where(eq(usersTable.id, id)).limit(1)
+            const verifyPassword = await $verifyPassword(newPassword, newUser[0].passwordHash)
+
+            expect(verifyPassword).toBe(true)
+
+            expect(body).toEqual({
+                id,
+                username: username.toLowerCase(),
+                createdAt: createdAt.toISOString(),
+                updatedAt: expect.not.stringMatching(updatedAt.toISOString()),
+            })
+
+            expect(statusCode).toBe(200)
+
+            await user.delete()
+        })
+
+        test('shouldn\'t change password with an invalid old password', async () => {
+            const user = await $mockUser()
+            const { cookies } = await user.createCookie()
+
+            const reqBody = { newPassword: generateID(6), oldPassword: 'invalidpasswordexample' }
+            const { body, statusCode } = await $mockHttp(usersRoute).patch(reqBody, { cookies });
+
+            expect(body).toEqual({
+                cause: { oldPassword: "Invalid Password" },
+                message: 'Invalid Request'
+            })
+            expect(statusCode).toBe(400)
+
+            await user.delete()
+        })
+
+        test('shouldn\'t change password without an old password', async () => {
+            const user = await $mockUser()
+            const { cookies } = await user.createCookie()
+
+            const reqBody = { newPassword: generateID(6) }
+            const { body, statusCode } = await $mockHttp(usersRoute).patch(reqBody, { cookies });
+
+            expect(body).toEqual({
+                cause: { oldPassword: "Required" },
+                message: 'Invalid Request'
+            })
+            expect(statusCode).toBe(400)
+
+            await user.delete()
+        })
+    })
+})
---
+++ __tests__/test-utils/mocks/data/mockUser.ts
@@ -0,0 +1,51 @@
+import { db } from "@/server/db";
+import { usersTable } from "@/server/db/schema";
+import { $hashPassword } from "@/server/utils/auth/auth";
+import { $createSession, $generateSessionToken } from "@/server/utils/auth/session";
+import $deleteFolder from "@/server/utils/file/deleteFolder";
+import { generateID } from "@/utils/lib/generateID";
+import { eq } from "drizzle-orm";
+import { mkdir } from "fs/promises";
+import { join } from "path";
+
+const SESSION_LOCAL_STORAGE_KEY = "session";
+
+export default async function $mockUser(username?: string, password: string = 'testpassword', createdAt?: Date) {
+    const passwordHash = await $hashPassword(password)
+
+    const date = createdAt || new Date()
+    const userData = await db.insert(usersTable).values({
+        id: generateID(),
+        username: (username || generateID(5).toLowerCase()),
+        passwordHash,
+        createdAt: date,
+        updatedAt: date,
+    }).returning()
+    const user = userData[0]
+
+    const dir = join('public', 'users', user.id)
+    await mkdir(dir, { recursive: true })
+
+    async function deleteUser() {
+        await $deleteFolder(dir)
+        await db.delete(usersTable).where(eq(usersTable.id, user.id)).limit(1)
+    }
+
+    async function createCookie() {
+        const token = $generateSessionToken();
+        const session = await $createSession(token, user.id);
+        const cookies = { [SESSION_LOCAL_STORAGE_KEY]: token }
+
+        return { token, session, cookies }
+    }
+
+    return {
+        userData: {
+            ...user,
+            password
+        },
+        userDir: dir,
+        delete: deleteUser,
+        createCookie
+    }
+}
