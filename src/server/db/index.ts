7b80bf1271da9f0cf8aa7007b67b755128346039
khalidibnwalid <kh.hawajreh@gmail.com>
2025-03-14 11:35:27 +0300

    chore: update default database path in db - for consistency with the one in drizzle.config

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,4 +1,4 @@
 import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 
-export const db = drizzle(new Database(process.env.DATABASE_PATH || 'sqlite.db'))
+export const db = drizzle(new Database(process.env.DATABASE_PATH || 'db/sqlite.db'))
